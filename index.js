d45d671ed01c28a754b89a45725af46f8a775cc1
Nicolas Perriault <nicolas@perriault.net>
2024-01-08 14:50:54 +0100

    Install fetch polyfill for older node installations.

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 const masto = require("masto");
 const recipes = require("./recipes");
 
+const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+if (!globalThis.fetch) {
+  globalThis.fetch = fetch;
+  globalThis.Headers = fetch.Headers;
+}
+
 const client = masto.createRestAPIClient({
   url: process.env.URL,
   accessToken: process.env.TOKEN,
