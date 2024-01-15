cace19979c4b3b58ffdf4ebeb89dd06a1fff6ca1
Nicolas Perriault <nicolas@perriault.net>
2024-01-15 17:42:55 +0100

    Add recipe steps.

--- index.js
+++ index.js
@@ -11,19 +11,34 @@ const client = masto.createRestAPIClient({
 const openai = new OpenAI();
 
 async function tootNewRecipe() {
-  const recipe = recipes.run();
-  const imageUrl = await textToImage(recipe);
+  const recipeName = recipes.run();
+  const imageUrl = await textToImage(recipeName);
   const remoteFile = await fetch(imageUrl);
   const attachment = await client.v2.media.create({
     file: await remoteFile.blob(),
-    description: recipe,
+    description: recipeName,
   });
+  const recipeSteps = await generateRecipeSteps(recipeName);
   const { url } = await client.v1.statuses.create({
-    status: recipe,
+    status: `${recipeName}\n\n${recipeSteps}`,
     visibility: process.env.VISIBILITY,
     mediaIds: [attachment.id],
   });
-  console.log(`New recipe posted: ${recipe} ${url}`);
+  console.log(`New recipe posted: ${recipeName} ${url}`);
+}
+
+async function generateRecipeSteps(recipe) {
+  const maxLength = 500 - recipe.length - 5;
+  const response = await openai.chat.completions.create({
+    model: "gpt-4",
+    messages: [
+      {
+        role: "system",
+        content: `Décris-moi les étapes pour réaliser la recette "${recipe}" en moins de ${maxLength} caractères`,
+      },
+    ],
+  });
+  return response.choices[0].message.content;
 }
 
 async function textToImage(prompt) {
