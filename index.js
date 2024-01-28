2ebd66aff68a990d7309f9814b65f6cc9f2e07cb
Nicolas Perriault <nicolas@perriault.net>
2024-01-29 00:01:25 +0100

    Back to basic (prompt).

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ async function tootNewRecipe() {
   const { name: recipeName, kind } = recipes.run();
   console.debug("Recette:", recipeName);
   const recipeSteps = await generateRecipeSteps(recipeName);
-  const imagePrompt = `Une photo réaliste du plat gastronomique “${recipeName}” prêt à être dégusté sur une table de restaurant`;
+  const imagePrompt = `Une photo du plat “${recipeName}” et quelques ingrédients et ustensiles disposés autour`;
   const imageUrl = await textToImage(imagePrompt);
   const remoteFile = await fetch(imageUrl);
   const attachment = await client.v2.media.create({
